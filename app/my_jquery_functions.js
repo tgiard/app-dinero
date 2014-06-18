6e0cea8b0a9595b0434b6919fddebdab891f0a43
Thibault Giard <tgiard@thibault-laptop.(none)>
2014-06-18 16:23:34 +0200

    premiers tests

---
+++ app/my_jquery_functions.js
@@ -0,0 +1,13 @@
+
+
+$(document).ready(function(){
+  $(".btn-danger").click(function(){
+    $("#inputSuccess2").hide();
+  });
+});
+
+$(document).ready(function(){
+  $(".btn-success").click(function(){
+    $("#inputSuccess2").show();
+  });
+});
