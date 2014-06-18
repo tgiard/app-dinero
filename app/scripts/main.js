939966250fd93e31dadc992613ec6ba035c4f4d7
Thibault Giard <tgiard@thibault-laptop.(none)>
2014-06-18 16:27:49 +0200

    premiers tests

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,28 @@
 console.log('\'Allo \'Allo!');
+
+$(document).ready(function(){
+  $(".btn-danger").click(function(){
+    $("#inputSuccess2").hide(1000);
+  });
+
+  $(".btn-success").click(function(){
+    $("#inputSuccess2").show();
+  });
+
+$("input").focus(function(){
+  $(this).css("background-color","#cccccc");
+});
+
+$("#flip").click(function(){
+  $("#mainPanel").slideToggle();
+if($("#flip").text() == 'Show') {
+        $(this).text('Hide');
+    } else {
+        $(this).text('Show');
+    }
+});
+
+$("input").blur(function(){
+  $(this).css("background-color","#ffffff");
+});
+});
